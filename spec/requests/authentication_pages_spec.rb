9b605eb3621af0ae45ac69c5dba8299dfb5dd846
Ragedyman <d3sch41n@yahoo.com>
2015-03-07 09:24:51 -0600

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -84,6 +84,18 @@ end
   end
  end
 
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_url) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_url) }
+        end
+      end
+
   describe "when attempting to visit a protected page" do
     before do
      visit edit_user_path(user)
@@ -126,12 +138,21 @@ end
    specify { response.should redirect_to(signin_path) }
     end
    end
-  end
 
   describe "visiting the user index" do
    before { visit users_path }
    it { should have_selector('title', text: 'Sign in') }
    end
+
+    describe "visiting the following page" do
+      before { visit following_user_path(user) }
+      it { should have_selector('title', text: 'Sign in') }
+    end
+
+    describe "visiting the followers page" do
+      before { visit followers_user_path(user) }
+      it { should have_selector('title', text: 'Sign in') }
+    end
   end
 
   describe "as wrong user" do
@@ -162,4 +183,4 @@ end
    end
   end
  end
-
+end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,6 +10,7 @@ subject { page }
 
  describe "Home page" do
    before { visit root_path }
+
    let(:heading) { 'Sample App' }
    let(:page_title) { '' }
 
@@ -31,7 +32,34 @@ end
      page.should have_selector("li##{item.id}", text: item.content)
   end
  end
-end
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
+    end
+  end
 
  describe "Help page" do
   before { visit help_path } 
